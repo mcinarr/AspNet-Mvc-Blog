e27a1909e9dc1ce838b53eb68c7df5a55f8be6ef
Mehmet <memethalil15@hotmail.com.tr>
2023-07-19 17:39:08 +0300

    aaaaa

---
+++ AppWebMvc/AppWebMvc/Controllers/UserControllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppWebMvc.Controllers.UserControllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
